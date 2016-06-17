4b16f3da98cc156d02fc48500ed7a3ff26743666
Nathan Shubert-Harbison <nathan@carterhales.com>
2016-06-17 14:12:44 -0700

    enable Modernizr.mq by default

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
           shiv: true,
           printshiv: false,
           load: true,
-          mq: false,
+          mq: true,
           cssclasses: true
         },
         extensibility: {
